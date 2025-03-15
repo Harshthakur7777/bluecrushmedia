21f5e7c4b72d17845db8c8634b378391e427c1ae
Harshthakur7777 <thakurharsh08032003@gmail.com>
2025-03-15 09:37:02 +0530

    Updated some files

--- app.js
+++ app.js
@@ -26,44 +26,16 @@ app.use((req, res, next) => {
     res.locals.error = req.flash('error');
     next();
 });
-app.post('/contact', async (req, res) => {
-    try {
-        const { name, email, message } = req.body;
 
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'thakurharsh08032003@gmail.com',
-                pass: 'gorp liqv zxly juow'
-            }
-        });
-
-        let mailOptions = {
-            from: email,
-            to: 'thakurharsh08032003@gmail.com',
-            subject: `Contact Form Submission from ${name}`,
-            text: message
-        };
-
-        await transporter.sendMail(mailOptions);
-        req.flash('success', 'Message sent successfully!');
-        res.redirect('/');
-
-    } catch (error) {
-        console.error("❌ Error sending email:", error);
-        req.flash('error', 'Failed to send message. Please try again.');
-        res.redirect('/');
-    }
-});
 
 
 // Start Server
-app.listen(3000, () => {
+app.listen(8080, () => {
     console.log('Server running on port 3000');
 })
 app.get('/',(req,res,next)=>{
     res.render('index')
 })
-app.listen(8000,()=>{
-    console.log('app is listening')
+app.all('*',(req,res,next)=>{
+    res.render('index')
 })
