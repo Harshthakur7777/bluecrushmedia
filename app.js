1d30ce1a3f5ca2a24907f3a0b255c22c5ba68227
Harshthakur7777 <thakurharsh08032003@gmail.com>
2025-03-15 10:09:28 +0530

    Updated some files

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 
 // Start Server
-const PORT = 8000; // or the port you actually want
+const PORT = 3000; // or the port you actually want
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
